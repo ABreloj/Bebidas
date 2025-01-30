089ef007078b65e6419bf8875b628bc54bcf5dfb
Abraham <abrahamperezquiroz0@gmail.com>
2025-01-30 16:32:53 -0600

    02 Header component ald layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+
+import Header from "../components/Header"
 export default function HomePAGE(){
     return(
         <h1>Pagina Home</h1>
--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,13 @@
 import {  BrowserRouter, Routes, Route  } from "react-router-dom"
 import HomePAGE from "./pages/HomePage";
 import FavoritePage from "./pages/favoritePage";
+import Layout from './Layout/Layout';
 
 export default function AppRouter(){
     return (
         <BrowserRouter>
             <Routes>
+                <Route element={ <Layout />} />
                 <Route path="/" element= { <HomePAGE/> } />
                 <Route path="Favoritos/" element= { <FavoritePage/> } />
             </Routes>
