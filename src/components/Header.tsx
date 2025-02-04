e7ef31f02149113dc0ba82c13b8dd5d41e6b8747
Abraham <abrahamperezquiroz0@gmail.com>
2025-02-04 12:43:01 -0600

    04 form tearch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
-import { NavLink } from "react-router-dom";
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
+
+    const { pathname } = useLocation()
+
+    const isHome = useMemo(() => pathname === '/' , [pathname])
+
   return (
     <header className="bg-slate-800">
         <div className="mx-auto container px-5 py-12">
@@ -22,6 +28,33 @@ export default function Header() {
                 </nav>
 
             </div>
+
+            { isHome && (
+                <form className="md:w-1/2 2x1:w-1/3 bg-orange-500 my-32 p-11 round" >
+                    <div className="space-y-4">
+                        <label htmlFor="ingredient" className="block text-white uppercase font-extrabold text-lg">
+                            Name o Ingredients 
+                        </label>
+
+                        <input id="ingredients" type="text" name="ingredients" className='p-3 w-full rounded-lg focus:outline-none' placeholder='Nombre o Ingredientes. Ej. La Cocacola'/>
+
+                    </div>
+                    <div className="space-y-4">
+                        <label htmlFor="ingredient" className="block text-white uppercase font-extrabold text-lg">
+                            La categoria 
+                        </label>
+
+                        <select id="ingredients" name="category" className='p-3 w-full rounded-lg focus:outline-none'>
+                            <option value="">Selecionar aqui</option>
+                        </select>
+
+                    </div>
+
+                    <input type='submit' value='Buscar Recetas' className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase" />   
+
+                </form>
+            )}
+
         </div>
     </header>
   )
